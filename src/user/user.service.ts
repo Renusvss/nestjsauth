54d4900394401c0e4416f1d882347d043356d5aa
Venkata Sri Sai Renu <renu.venkata@proclink.local>
2022-12-01 17:52:49 +0530

    adding auth

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/types/user';
+import { RegisterDTO } from './register.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { LoginDTO } from 'src/auth/login.dto';
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+  async create(RegisterDTO: RegisterDTO) {
+    const { email } = RegisterDTO;
+    const user = await this.userModel.findOne({ email });
+    if (user) {
+      throw new HttpException('user already exists', HttpStatus.BAD_REQUEST);
+    }
+    const createdUser = new this.userModel(RegisterDTO);
+    await createdUser.save();
+    return this.sanitizeUser(createdUser);
+  }
+  // return user object without password
+  sanitizeUser(user: User) {
+    const sanitized = user.toObject();
+    delete sanitized['password'];
+    return sanitized;
+  }
+}
